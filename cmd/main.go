99c23068463ef5a1628f445d37dcfebaa988e391
Александр Шмаков <st085954@student.spbu.ru>
2025-04-08 23:51:29 +0300

    added links crawling

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,96 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+var queuePath string = "././queue.json"
+
+type settings struct {
+	Mode       bool     `json:"spider_mode"`
+	ToDownload []string `json:"toDownload"`
+}
+
+func (s *settings) SetSettings(fileName string) {
+	jsonFile, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Ошибка открытия файла:", err)
+		return
+	}
+	defer jsonFile.Close()
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println("Ошибка чтения файла: ", err)
+		return
+	}
+
+	err = json.Unmarshal(byteValue, &s)
+	if err != nil {
+		fmt.Println("Ошибка декодирования JSON: ", err)
+	}
+}
+
+func fetchHTML(url string) (*html.Node, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("error: status code %d", resp.StatusCode)
+	}
+
+	return html.Parse(resp.Body)
+}
+
+func extractLinks(n *html.Node, baseURL string) []string {
+	var links []string
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					link := a.Val
+					if !strings.HasPrefix(link, "http") {
+						link = baseURL + link
+					}
+					links = append(links, link)
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(n)
+	return links
+}
 
 func main() {
-	fmt.Println("start project")
+	settings := &settings{}
+	settings.SetSettings(queuePath)
+
+	for _, url := range settings.ToDownload {
+		fmt.Println("Start with URL: ", url)
+
+		node, err := fetchHTML(url)
+		if err != nil {
+			fmt.Println("Error fetching HTML: ", err)
+			return
+		}
+
+		links := extractLinks(node, url)
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.23.0
+
+require golang.org/x/net v0.39.0 // indirect
